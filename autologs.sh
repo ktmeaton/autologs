6ebcc313dbbdfaf84417efb69413bd9624b0a4aa
Katherine Eaton <ktmeaton@gmail.com>
2021-02-18 11:53:54 -0500

    self contained commits run

--- autologs.sh
+++ autologs.sh
@@ -63,8 +63,30 @@ Version()
 
 Commits()
 {
-  # Execute the commit history script
-  ${AUTOLOGS_DIR}/"scripts/commits.sh"
+  # Fetch the remote origin url
+  strip="origin\t\| (push)\|\.git"
+  origin=`git remote -v | grep -E "origin.*push" | sed "s/$strip//g"`
+
+  # Get the base url for commits
+  base="$origin/commit"
+
+  # Counter for commits to print
+  i=0;
+
+  git log --pretty=oneline --abbrev-commit ${OLD_VER}..${NEW_VER} | \
+    while read line;
+    do
+      # Stop printing if max commits has been reached
+      if [[ $i -ge $MAX_COMMITS && $MAX_COMMITS != -1 ]]; then
+        break
+      fi
+      hash=`echo $line | cut -d " " -f 1`
+      msg=`echo $line | sed "s/$hash //g"`
+      echo -e "* [\`\`\`$hash\`\`\`]($base/$hash) $msg";
+
+      # Increment commit counter
+      i=`expr $i + 1`
+    done;
 }
 
 Release()
