e000fd010bd2611ca9d38dd127dd16431efd2325
Katherine Eaton <ktmeaton@gmail.com>
2021-02-18 09:51:58 -0500

    remove hard coded variables

--- scripts/notes_commits.sh
+++ scripts/notes_commits.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+#---------------------------------------------------
+# Input Arguments
+
+# Old version to compare to (default is first commit)
 OLD_VER=$1
-# Default is the first commit
-first_commit=`git log -r | head -n 1 | cut -d " " -f2`
-OLD_VER="$OLD_VER:=${first_commit}}"
+first_commit=`git log --reverse | head -n 1 | cut -d " " -f2`
+OLD_VER=${OLD_VER:=${first_commit}}
 
-echo $OLD_VER
-exit
-
+# New version to compare to, default is HEAD
 NEW_VER=$2
-# Default number of commits to print is 20 (use -1 for infinite)
+NEW_VER=${NEW_VER:="HEAD"}
+
+# Number of commits to print (default is 20, use -1 for infinite)
 MAX_COMMITS=$3
 MAX_COMMITS="${MAX_COMMITS:=20}"
+
+#---------------------------------------------------
+# Processing
 
 # Fetch the remote origin url
 origin=`git remote -v | grep -E "origin.*push" | sed 's/origin\t//g' | sed 's/ (push)//g' | sed 's/\.git//g'`
@@ -24,12 +30,13 @@ i=0;
 git log --pretty=oneline --abbrev-commit ${OLD_VER}..${NEW_VER} | \
   while read line;
   do
+    # Stop printing if max commits has been reached
     if [[ $i -ge $MAX_COMMITS && $MAX_COMMITS != -1 ]]; then
       break
     fi
     hash=`echo $line | cut -d " " -f 1`
     msg=`echo $line | sed "s/$hash //g"`
-    echo -e "* [\`\`\`$hash\`\`\`]($BASE/$hash) $msg";
+    echo -e "* [\`\`\`$hash\`\`\`]($base/$hash) $msg";
 
     # Increment commit counter
     i=`expr $i + 1`
